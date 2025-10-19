d74a178eea10ec7eebb4e86f653ca8c66d2d9e0a
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2025-10-19 13:54:18 +0700

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+int main()
+{
+  int n=0;
+  int z=0;
+  int x;
+  printf("введите число для сложения:\n");
+  scanf("%d", &x);
+  while (n++<x)
+    z=n+z;
+    printf("%d\n", z);
+  return 0;
+}

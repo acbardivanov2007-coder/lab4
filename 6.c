7135a5f6da2895d964f664cc15015c16f5d91a04
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2025-10-23 16:17:29 +0700

    Replace variable x with day for input and loop

--- 6.c
+++ 6.c
@@ -3,10 +3,10 @@ int main()
 {
   int mon=0;
   int z=0;
-  int x;
+  int day;
   printf("введите количество дней для сложения денег в квадрате:\n");
-  scanf("%d", &x);
-  while (mon++<x)
+  scanf("%d", &day);
+  while (mon++<day)
          z=(mon*mon+z);
          printf("вот ваша сумма %d\n", z);
   return 0;

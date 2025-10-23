a472baa2b1a32a1a2ea0f9d999b67bf3b7f90d9c
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2025-10-23 16:37:31 +0700

    Update 5.c

--- 5.c
+++ 5.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 int main()
 {
-  int n=0;
+  int mon=0;
   int z=0;
-  int x;
-  printf("введите число для сложения:\n");
-  scanf("%d", &x);
-  while (n++<x)
-    z=n+z;
+  int day;
+  printf("введите количество дней для сложения:\n");
+  scanf("%d", &day);
+  while (mon++<day)
+    z=mon+z;
     printf("%d\n", z);
   return 0;
 }

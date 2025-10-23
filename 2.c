354ad97d130d3e216ed17db271eb1137527c8b78
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2025-10-23 17:18:45 +0700

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main ()
 {
-  int n=0;
+  int num=0;
   int z=0;
   printf("выберите число:\n");
-  scanf("%d", &n);
-  z = n;
-  while (n <= z+10)
+  scanf("%d", &num);
+  z = num;
+  while (num <= z+10)
         {
-        printf("%d\n", n++);
+        printf("%d\n", num++);
         }
   return 0;
 }
